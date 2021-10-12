3415c641dd5e01a9e6675d49b2682350cc8eed9c
Md-Alamin-hub <86272640+Md-Alamin-hub@users.noreply.github.com>
2021-10-12 23:02:32 +0600

    useParams hook added and dynamic route set

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./Home";
+import Booking from "./Pages/Booking/Booking";
 import NotFounds from "./Pages/NotFound/NotFounds";
 
 function App() {
@@ -13,6 +14,9 @@ function App() {
           </Route>
           <Route exact path="/home">
             <Home></Home>
+          </Route>{" "}
+          <Route path="/booking/:serviceId">
+            <Booking></Booking>
           </Route>
           <Route path="*">
             <NotFounds></NotFounds>
--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,8 +1,11 @@
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Service.css";
 
 const Service = (props) => {
-  const { name, description, price, img } = props.service;
+  const { id, name, description, price, img } = props.service;
   return (
     <div className="service">
       <div className="img-service">
@@ -12,6 +15,11 @@ const Service = (props) => {
         <h3>{name}</h3>
         <h5> ${price} </h5>
         <p>{description} </p>
+        <Link to={`/booking/${id}`}>
+          <button className="btn btn-warning">
+            <FontAwesomeIcon icon={faArrowRight} /> Book {name.toLowerCase()}
+          </button>
+        </Link>
       </div>
     </div>
   );
